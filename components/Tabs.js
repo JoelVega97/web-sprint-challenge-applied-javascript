983dc66183aef93237a8cb453e9151e113df250a
joel vega <joelvega97@yahoo.com>
2020-08-07 13:26:49 -0400

    finished the MVP

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,6 +1,6 @@
 // STEP 2: Create tabs
 // -----------------------
-// Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
+// Using axios send a GET request to the address: 
 // Once the data is resolved use console logs or breakpoints to review the structure.
 // Iterate over the topics creating a new tab for each topic, and appending it to the DOM
 // under the div.topics element.
@@ -9,3 +9,23 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+const topicsContainers = document.querySelector('.topics')
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(function (res){
+        const topicNames = res.data.topics
+        topicNames.forEach(element => {
+            let topic = document.createElement('div')
+            topic.className = 'tab'
+            topic.textContent = `${element}`
+            topicsContainers.appendChild(topic)
+            
+            topic.addEventListener('click', function(event){
+                topic.classList.toggle('active-tab')
+            })
+        });
+    })
+    .catch(function(error){
+        console.log('dammit')
+    })
